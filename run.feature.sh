cd43567fe84d58328d21adc7b853a6f8aa2a4f2f
Yu-Heng Lei <yuhengl1@andrew.cmu.edu>
2017-02-01 23:26:14 -0500

    Split part of run.fearture.sh into run.cluster.sh

--- run.feature.sh
+++ run.feature.sh
@@ -38,30 +38,3 @@ done
 # You may find the number of MFCC files mfcc/*.mfcc.csv is slightly less than the number of the videos. This is because some of the videos
 # don't hae the audio track. For example, HVC1221, HVC1222, HVC1261, HVC1794 
 
-# In this part, we train a clustering model to cluster the MFCC vectors. In order to speed up the clustering process, we
-# select a small portion of the MFCC vectors. In the following example, we only select 20% randomly from each video. 
-echo "Pooling MFCCs (optional)"
-python scripts/select_frames.py list/train.video 0.2 select.mfcc.csv || exit 1;
-
-# now trains a k-means model using the sklearn package
-echo "Training the k-means model"
-python scripts/train_kmeans.py select.mfcc.csv $cluster_num kmeans.${cluster_num}.model || exit 1;
-
-# Now that we have the k-means model, we can represent a whole video with the histogram of its MFCC vectors over the clusters. 
-# Each video is represented by a single vector which has the same dimension as the number of clusters. 
-echo "Creating k-means cluster vectors"
-python scripts/create_kmeans.py kmeans.${cluster_num}.model $cluster_num list/all.video || exit 1;
-
-# Now you can see that you get the bag-of-word representations under kmeans/. Each video is now represented
-# by a {cluster_num}-dimensional vector.
-
-# Now we generate the ASR-based features. This requires a vocabulary file to available beforehand. Each video is represented by
-# a vector which has the same dimension as the size of the vocabulary. The elements of this vector are the number of occurrences 
-# of the corresponding word. The vector is normalized to be like a probability. 
-# You can of course explore other better ways, such as TF-IDF, of generating these features.
-echo "Creating ASR features"
-mkdir -p asrfeat
-python scripts/create_asrfeat.py vocab list/all.video || exit 1;
-
-# Great! We are done!
-echo "SUCCESSFUL COMPLETION"
