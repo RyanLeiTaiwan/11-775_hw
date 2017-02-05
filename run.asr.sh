7a895ff3a6e0ec0b20538535eb3ddd1578305e45
Yu-Heng Lei <yuhengl1@andrew.cmu.edu>
2017-02-05 15:15:24 -0500

    Split list/all.video into 10 parts to run on 10 VMs

--- run.asr.sh
+++ run.asr.sh
@@ -5,22 +5,24 @@
 # Paths to different tools and data
 eesen_path=$HOME/tools/eesen-offline-transcriber
 audio_path=$HOME/hw1/audio
-asr_output_path=$HOME/hw1/asrtxt
 
 # Run ASR transcriber (speech2text)
 echo "Cleaning ASR transcrition output"
+echo "rm -rf $eesen_path/build/output/*"
 rm -rf $eesen_path/build/output/*
 
 echo "Running ASR transcriber (speech2text)"
-for file in $(ls $audio_path/*)
+# Break the all.video list into multiple parts and run on different VMs
+for file in $(cat list/all.video.1)
 do
-    echo $eesen_path/speech2text.sh $file
-    $eesen_path/speech2text.sh $file
+    # echo "$eesen_path/speech2text.sh $audio_path/$file.wav"
+    $eesen_path/speech2text.sh $audio_path/$file.wav
 done
 
 echo "Copying ASR transcriptions (txt) to HW directory"
 mkdir -p asrtxt
-cp $eesen_path/build/output/*.txt $asr_output_path
+echo "cp $eesen_path/build/output/*.txt asrtxt"
+cp $eesen_path/build/output/*.txt asrtxt
 
 # Now we generate the ASR-based features. This requires a vocabulary file to available beforehand. Each video is represented by
 # a vector which has the same dimension as the size of the vocabulary. The elements of this vector are the number of occurrences 
