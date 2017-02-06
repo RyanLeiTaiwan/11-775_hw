74f88035c4144bdf79ffc586a719aa9f438491f1
Yu-Heng Lei <yuhengl1@andrew.cmu.edu>
2017-02-06 00:20:03 -0500

    Spllit run.med.sh into run.med.mfcc.sh and run.med.asr.sh

--- run.med.asr.sh
+++ run.med.asr.sh
@@ -15,23 +15,6 @@ map_path=/home/ubuntu/tools/mAP
 export PATH=$opensmile_path:$speech_tools_path:$ffmpeg_path:$map_path:$PATH
 export LD_LIBRARY_PATH=$ffmpeg_path/libs:$opensmile_path/lib:$LD_LIBRARY_PATH
 
-echo "#####################################"
-echo "#       MED with MFCC Features      #"
-echo "#####################################"
-mkdir -p mfcc_pred
-# iterate over the events
-feat_dim_mfcc=200
-for event in P001 P002 P003; do
-  echo "=========  Event $event  ========="
-  # now train a svm model
-  python scripts/train_svm.py $event "kmeans/" $feat_dim_mfcc mfcc_pred/svm.$event.model || exit 1;
-  # apply the svm model to *ALL* the testing videos;
-  # output the score of each testing video to a file ${event}_pred 
-  python scripts/test_svm.py mfcc_pred/svm.$event.model "kmeans/" $feat_dim_mfcc mfcc_pred/${event}_pred || exit 1;
-  # compute the average precision by calling the mAP package
-  ap list/${event}_test_label mfcc_pred/${event}_pred
-done
-
 echo ""
 echo "#####################################"
 echo "#       MED with ASR Features       #"
