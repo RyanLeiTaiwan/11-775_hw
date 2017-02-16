2147176ed431879b01ad058497ed4902861adc39
Yu-Heng Lei <yuhengl1@andrew.cmu.edu>
2017-02-16 01:39:35 -0500

    Update pca.py for AWS run

--- scripts_hw2/pca.py
+++ scripts_hw2/pca.py
@@ -5,7 +5,7 @@ from sklearn.decomposition import PCA
 import cPickle
 import sys
 
-# Run KPCA to reduce dimensionality of imtraj feature
+# Run PCA to reduce dimensionality of imtraj feature
 
 if __name__ == '__main__':
     if len(sys.argv) != 5:
@@ -22,33 +22,33 @@ if __name__ == '__main__':
     np.random.seed(11775)
 
     # # Load the sparse feature file
-    # X = []
-    # # for video in ['HVC51']:
-    # for video in video_list:
-    #     vec = [0] * feat_dim
-    #     filename = feat_dir + '/' + video + feat_suffix
-    #     print filename
-    #     # Only one line per video
-    #     feat_line = open(filename).readlines()[0].strip()
-    #     # print line
-    #     fields = feat_line.split(' ')
-    #     for dim in fields:
-    #         tok = dim.split(':')
-    #         # feature file is 1-based indexing
-    #         vec[int(tok[0]) - 1] = float(tok[1])
-    #     X.append(vec)
+    X = []
+    # for video in ['HVC51']:
+    for video in video_list:
+        vec = [0] * feat_dim
+        filename = feat_dir + '/' + video + feat_suffix
+        print filename
+        # Only one line per video
+        feat_line = open(filename).readlines()[0].strip()
+        # print line
+        fields = feat_line.split(' ')
+        for dim in fields:
+            tok = dim.split(':')
+            # feature file is 1-based indexing
+            vec[int(tok[0]) - 1] = float(tok[1])
+        X.append(vec)
 
-    # X = np.array(X)
-    # print "X.shape: " + str(X.shape)
+    X = np.array(X)
+    print "X.shape: " + str(X.shape)
 
     # # Temp: dump as pickle
     # ftmp = open('imtraj.pk', 'wb')
     # cPickle.dump(X, ftmp)
 
-    # Temp: load from pickle
-    ftmp = open('imtraj.pk', 'r')
-    X = cPickle.load(ftmp)
-    print 'Finished loading pk'
+    # # Temp: load from pickle
+    # ftmp = open('imtraj.pk', 'r')
+    # X = cPickle.load(ftmp)
+    # print 'Finished loading pk'
 
     # Examine the % of variance explained by principal components
     pca = PCA()
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     # Set the target dimensionality to the one retaining 0.99 variance
     pca_dim = sum(var < 0.99) + 1
 
-    # Reduce dimensionality (sklearn requires to fit again)
+    # Reduce dimensionality (sklearn requires us to 'fit' again)
     pca.n_components = pca_dim
     X_pca = pca.fit_transform(X)
     print "X_pca.shape: " + str(X_pca.shape)
