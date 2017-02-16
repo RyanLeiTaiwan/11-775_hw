f526900bd5996b3b20f7710d879ded77ca1d0a6d
Yu-Heng Lei <yuhengl1@andrew.cmu.edu>
2017-02-16 01:20:28 +0000

    Download HW2 test videos, run ffmpeg

--- download_videos_hw2.sh
+++ download_videos_hw2.sh
@@ -4,23 +4,24 @@ local_video_path=$HOME/video_local
 mkdir -p $local_video_path
 
 # Only download positive videos of each class
-for class in P001 P002 P003
-do
-    echo "Downloading positive training videos for class" $class"..."
-    mkdir -p $local_video_path/train_pos_$class
-    for file in $(cat list_hw2/train_$class)
-    do  
-        echo $file".mp4"
-        cp $s3_video_path/$file.mp4 $local_video_path/train_pos_$class
-    done
+#for class in P001 P002 P003
+#do
+#    echo "Downloading positive training videos for class" $class"..."
+#    mkdir -p $local_video_path/train_pos_$class
+#    for file in $(cat list_hw2/train_$class)
+#    do  
+#        echo $file".mp4"
+#        cp $s3_video_path/$file.mp4 $local_video_path/train_pos_$class
+#    done
+#
+#done
 
-    # Testing video list is not yet available
-    #echo "Downloading positive testing videos for class" $class"..."
-    #mkdir -p $local_video_path/test_pos_$class
-    #for file in $(cat list/test | grep $class | awk '{print $1}')
-    #do  
-    #    echo $file".mp4"
-    #    cp $s3_video_path/$file.mp4 $local_video_path/test_pos_$class
-    #done
+# We don't know the class labels of test videos
+echo "Downloading ALL test videos..."
+mkdir -p $local_video_path/test
+for file in $(cat list_hw2/test_hw2)
+do  
+    echo $file".mp4"
+    cp $s3_video_path/$file.mp4 $local_video_path/test
 done
 
--- scripts_hw2/run.ffmpeg.sh
+++ scripts_hw2/run.ffmpeg.sh
@@ -16,8 +16,9 @@ height=480
 
 mkdir -p temp keyframe_$cut
 
-for line in $(cat list_hw2/train_dev.video)
 #for line in HVC51 
+#for line in $(cat list_hw2/train_dev.video)
+for line in $(cat list_hw2/test_hw2)
 do
     ffmpeg -y -ss 0 -i $video_path/$line.mp4 -strict experimental -t $cut -r 15 -vf scale=$width"x"$height,setdar=4:3 temp/tmp_$cut.mp4
     mkdir -p keyframe_$cut/$line
