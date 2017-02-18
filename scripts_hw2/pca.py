a7f9e7de26a04459b02ee587fd516be0b875c3b6
Yu-Heng Lei <yuhengl1@andrew.cmu.edu>
2017-02-18 13:06:51 -0500

    PCA now has both training and testing modes. Dump/Load PCA model in training/testing mode

--- scripts_hw2/pca.py
+++ scripts_hw2/pca.py
@@ -1,5 +1,3 @@
-#!/bin/python
-
 import numpy as np
 from sklearn.decomposition import PCA
 import cPickle
@@ -8,22 +6,41 @@ import sys
 # Run PCA to reduce dimensionality of imtraj feature
 
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        print 'Usage: {0} video_list feat_dir feat_dim pca_path'.format(sys.argv[0])
+    # Use 'mode' argument to determine training / testing mode
+    if len(sys.argv) != 7:
+        print 'Usage: {0} mode video_list feat_dir feat_dim pca_file pca_model'.format(sys.argv[0])
+        print 'mode: "train" (pca_model as output) or "test" (pca_model as input)'
         exit(1)
+    if sys.argv[1] == 'train':
+        mode = 'TRAINING'
+    elif sys.argv[1] == 'test':
+        mode = 'TESTING'
+    else:
+        print 'Invalid PCA mode!'
+        print 'Usage: {0} mode video_list feat_dir feat_dim pca_file pca_model'.format(sys.argv[0])
+        print 'mode: "train" (pca_model as output) or "test" (pca_model as input)'
 
-    video_list = open(sys.argv[1]).read().splitlines()
-    feat_dir = sys.argv[2]
     feat_suffix = '.spbof'
-    feat_dim = int(sys.argv[3])
-    pca_path = sys.argv[4]
+    video_list = open(sys.argv[2]).read().splitlines()
+    feat_dir = sys.argv[3]
+    feat_dim = int(sys.argv[4])
+    pca_file = sys.argv[5]
+    pca_model = sys.argv[6]
 
     # Set a fixed random seed so we can reproduce the results
     np.random.seed(11775)
 
+    print '====  PCA ' + mode + ' mode  ===='
+    # Testing mode: Load PCA model
+    if mode == 'TESTING':
+        print 'Loading existing PCA model ' + pca_model
+        fmodel = open(pca_model, 'rb')
+        pca = cPickle.load(fmodel)
+        fmodel.close()
+
     print 'Loading sparse feature files...'
     X = []
-    # for video in ['HVC51']:
+    # for video in ['HVC51', 'HVC72', 'HVC285']:
     for video in video_list:
         vec = [0] * feat_dim
         filename = feat_dir + '/' + video + feat_suffix
@@ -50,25 +67,37 @@ if __name__ == '__main__':
     # X = cPickle.load(ftmp)
     # print 'Finished loading pk'
 
-    print 'Running PCA to examine the variance explained...'
-    # Examine the % of variance explained by principal components
-    pca = PCA()
-    pca.fit(X)
-    var = np.cumsum(pca.explained_variance_ratio_)
-    for ratio in [0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 0.98, 0.99]:
-        print str(ratio) + ' variance explained by ' + str(sum(var < ratio) + 1) + ' components'
-    # Set the target dimensionality to the one retaining 0.99 variance
-    pca_dim = sum(var < 0.99) + 1
-    print 'target dimensionality: ' + str(pca_dim)
+    if mode == 'TRAINING':
+        print 'Running PCA training to examine the variance explained...'
+        # Examine the % of variance explained by principal components
+        pca = PCA()
+        pca.fit(X)
+        var = np.cumsum(pca.explained_variance_ratio_)
+        for ratio in [0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 0.98, 0.99]:
+            print str(ratio) + ' variance explained by ' + str(sum(var < ratio) + 1) + ' components'
+        # Set the target dimensionality to the one retaining 0.99 variance
+        pca_dim = sum(var < 0.99) + 1
+        print 'target dimensionality: ' + str(pca_dim)
 
-    # Reduce dimensionality (sklearn requires us to 'fit' again)
-    print 'Running PCA again to reduce dimensionality...'
-    pca.n_components = pca_dim
-    X_pca = pca.fit_transform(X)
+        # Reduce dimensionality (sklearn requires us to 'fit' again after resetting n_components)
+        print 'Running PCA training again + testing to reduce dimensionality...'
+        pca.n_components = pca_dim
+        X_pca = pca.fit_transform(X)
+
+        # Training mode: also output PCA model file
+        fmodel = open(pca_model, 'wb')
+        cPickle.dump(pca, fmodel)
+        fmodel.close()
+
+    else:
+        # Testing mode: Should NOT 'fit' again
+        print 'Running PCA testing to reduce dimensionality...'
+        X_pca = pca.transform(X)
+
     print "X_pca.shape: " + str(X_pca.shape)
 
     # Output reduced feature as a single pickle
-    print 'Output reduced feature as a single pickle: ' + pca_path
-    fout = open(pca_path, 'wb')
+    print 'Output reduced feature as a single pickle: ' + pca_file
+    fout = open(pca_file, 'wb')
     cPickle.dump(X_pca, fout)
     fout.close()
--- scripts_hw2/run.pca.sh
+++ scripts_hw2/run.pca.sh
@@ -2,11 +2,22 @@
 
 # Run PCA to reduce dimension of imtraj feature
 
-video_list=../list_hw2/train_dev.video
+train_video_list=../list_hw2/train_dev.video
+test_video_list=../list_hw2/test_hw2
 feat_dir=../imtraj_local
 feat_dim=32768
-pca_output=../imtraj_pred/imtraj_pca.pk
+pca_path=../imtraj_pred
+train_pca_file=$pca_path/train.imtraj.pk
+test_pca_file=$pca_path/test.imtraj.pk
+# Dimensionality is NOT known until after training
+pca_dim=713
+pca_model=$pca_path/pca.imtraj.${pca_dim}.model
 
-mkdir -p ../imtraj_pred
-python2 pca.py $video_list $feat_dir $feat_dim $pca_output || exit 1;
+mkdir -p $pca_path
+echo "Training PCA for training data dimensionality reduction"
+python2 pca.py train $train_video_list $feat_dir $feat_dim $train_pca_file $pca_model
+
+echo
+echo "Using existing PCA model for test data dimensionality reduction"
+python2 pca.py test $test_video_list $feat_dir $feat_dim $test_pca_file $pca_model
 
